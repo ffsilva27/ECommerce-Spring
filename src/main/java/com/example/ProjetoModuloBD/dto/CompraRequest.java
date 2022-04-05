f41cc290ff23c1b5190c9568577fa64bd69cb01e
ffsilva27 <filipeferreiradasilva@hotmail.com>
2022-04-05 10:01:47 -0300

    Created handle exception for compras.

--- src/main/java/com/example/ProjetoModuloBD/dto/CompraRequest.java
+++ src/main/java/com/example/ProjetoModuloBD/dto/CompraRequest.java
@@ -3,12 +3,17 @@ package com.example.ProjetoModuloBD.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter
 public class CompraRequest {
+    @NotNull
     private LocalDateTime data;
+    @NotEmpty
     private String cpf;
+    @NotEmpty
     private List<String> produtos;
 }
